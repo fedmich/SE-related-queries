27bbeff31c07ba73f880121bf8decd70a2926995
fed <fedmich@gmail.com>
2024-10-12 09:54:35 +0800

    schema

--- web/search-cp/related-queries/schema.sql
+++ web/search-cp/related-queries/schema.sql
@@ -9,3 +9,20 @@ COLLATE='utf8mb4_general_ci'
 ENGINE=InnoDB
 AUTO_INCREMENT=1
 ;
+
+
+
+
+CREATE TABLE `related_queries` (
+	`id` INT(11) NOT NULL AUTO_INCREMENT,
+	`query_id` INT(11) NOT NULL,
+	`related_query_text` VARCHAR(255) NOT NULL,
+	`created_at` TIMESTAMP NOT NULL DEFAULT '',
+	PRIMARY KEY (`id`),
+	INDEX `query_id` (`query_id`),
+	CONSTRAINT `related_queries_ibfk_1` FOREIGN KEY (`query_id`) REFERENCES `queries` (`id`) ON DELETE CASCADE
+)
+COLLATE='utf8mb4_general_ci'
+ENGINE=InnoDB
+AUTO_INCREMENT=1
+;
